b1a3d55b79af1589fed4b4b2bfcb445e135a4507
WuErPing <erpingwu@gmail.com>
2013-02-26 19:49:23 +0800

    std::srand(time(0)) influence effect of std::random_shuffle

---
+++ cpp/stl/random_shuffle.cc
@@ -0,0 +1,65 @@
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+// clang++ --std=c++11 random_shuffle.cc ; ./a.out
+
+std::vector<std::pair<std::string, uint16_t> > g_hosts = {
+    {"127.0.0.1", 50000}
+    ,{"127.0.0.1", 50001}
+    ,{"127.0.0.1", 50002}
+    ,{"127.0.0.1", 50003}
+    ,{"127.0.0.1", 50003}
+    ,{"127.0.0.1", 50004}
+    ,{"127.0.0.1", 50005}
+};
+
+
+void print_port_map(const map<uint16_t, int>& port_map) {
+  printf("map\n");
+  for(auto kv : port_map) {
+    printf("\tport=%d, count=%d\n", kv.first, kv.second);
+  }
+}
+
+void test_with_srand() {
+  map<uint16_t, int> port_map;
+  for(int i=0; i<100; ++i) {
+    auto hosts = g_hosts;
+    std::srand(time(0));
+    std::random_shuffle(hosts.begin(), hosts.end());
+    uint16_t port = hosts[0].second;
+    cout << port << endl;
+    if(port_map.end() == port_map.find(port))
+      port_map[port] = 0;
+    port_map[port] += port_map[port];
+  }
+  print_port_map(port_map);
+}
+
+void test() {
+  map<uint16_t, int> port_map;
+  for(int i=0; i<100; ++i) {
+    auto hosts = g_hosts;
+    // std::srand(time(0));
+    std::random_shuffle(hosts.begin(), hosts.end());
+    uint16_t port = hosts[0].second;
+    // cout << port << endl;
+    if(port_map.end() == port_map.find(port))
+      port_map[port] = 0;
+    port_map[port] += port_map[port];
+  }
+  print_port_map(port_map);
+}
+
+
+
+int main() {
+  test_with_srand();
+  test();
+}
+
+
