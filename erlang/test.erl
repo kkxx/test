28808e4e9a10d61084b341fea86095a39d610a7c
WuErPing <erpingwu@gmail.com>
2013-08-17 21:58:23 +0800

    spawn test

--- erlang/test.erl
+++ erlang/test.erl
@@ -1,5 +1,15 @@
 -module(test).
--export([start/0]).
+-export([start/0, say_something/2]).
+
+say_something(What, 0) ->
+    io:format("~p ---end--- ~n", [What]),
+    done;
+
+say_something(What, Times) ->
+    io:format("~p~n", [What]),
+    say_something(What, Times - 1).
 
 start() ->
-    io:format("Hello world~n").
+    io:format("---Hello world---~n"),
+    spawn(test, say_something, [hello, 3]),
+    spawn(test, say_something, [goodbye, 3]).
