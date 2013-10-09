ff4ca97cc5f377aea33985c8f842802b0ec95446
WuErPing <erpingwu@gmail.com>
2013-10-09 20:42:54 +0800

    sbt.bat

--- twitter_stack/twitter_finagle/sbt.bat
+++ twitter_stack/twitter_finagle/sbt.bat
@@ -1,4 +1,7 @@
 set SCRIPT_DIR=%~dp0
-java -Dhttp.proxyHost=127.0.0.1 -Dhttp.proxyPort=8087 -Dsbt.global.base=d:\sbt\.sbt -Dsbt.ivy.home=d:\sbt\.ivy2 -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -jar "%SCRIPT_DIR%sbt-launch.jar" %*
+
+java -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -jar "%SCRIPT_DIR%sbt-launch.jar" %*
+
+rem java -Dhttp.proxyHost=127.0.0.1 -Dhttp.proxyPort=8087 -Dsbt.global.base=d:\sbt\.sbt -Dsbt.ivy.home=d:\sbt\.ivy2 -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -jar "%SCRIPT_DIR%sbt-launch.jar" %*
 
 rem java  -Dsbt.global.base=d:\sbt\.sbt -Dsbt.ivy.home=d:\sbt\.ivy2 -Xmx512M -jar "%SCRIPT_DIR%sbt-launch.jar" %*
